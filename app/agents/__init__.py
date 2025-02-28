46080193916dd9de8550d83fd344fb630af3115c
xana-rahmani <xanarahmani@gmail.com>
2025-02-28 14:25:00 +0330

    init

--- app/agents/__init__.py
+++ app/agents/__init__.py
@@ -4,12 +4,12 @@ from app.agents.address_analyzer.agent import AddressAnalyzerAgent
 from app.agents.auditor.agent import ContractAuditorAgent
 from app.agents.base import BaseAgent
 from app.agents.educator.agent import BlockchainEducatorAgent
-from app.agents.tx_analyzer_agent import TxAnalyzerAgent
+from app.agents.transaction_analyzer.agent import TransactionAnalyzerAgent
 from app.config import Config
 
 agents: dict[str, Type[BaseAgent]] = {
     Config.BLOCKCHAIN_EDUCATOR_NAME.lower(): BlockchainEducatorAgent,
     Config.CONTRACT_AUDITOR_NAME.lower(): ContractAuditorAgent,
     Config.ADDRESS_ANALYZER_NAME.lower(): AddressAnalyzerAgent,
-    Config.TX_ANALYZER_NAME.lower(): TxAnalyzerAgent,
+    Config.TX_ANALYZER_NAME.lower(): TransactionAnalyzerAgent,
 }
