74ddf3122cda487b74e3f0191f8fd50a9ad08508
xana-rahmani <xanarahmani@gmail.com>
2025-02-28 14:48:00 +0330

    fixed get_transaction_details

--- app/services/transaction.py
+++ app/services/transaction.py
@@ -8,15 +8,10 @@ class TransactionService:
 
     def get_transaction_details(self, transaction_hash: str) -> str:
         params = {
-            "module": "account",
-            "action": "txlist",
-            "address": address,
+            "module": "proxy",
+            "action": "eth_getTransactionByHash",
+            "txhash": transaction_hash,
             "apikey": self.api_key,
-            "startblock": 0,
-            "endblock": 99999999,
-            "page": 1,
-            "offset": 1,
-            "sort": "asc",
         }
         response = requests.get(self.api_url, params=params)
         return response.json()
