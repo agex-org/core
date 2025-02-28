2fe66314a32a67fba89f92550e9780289f63eb3e
xana-rahmani <xanarahmani@gmail.com>
2025-02-28 14:49:03 +0330

    improved get_transaction_details

--- app/services/transaction.py
+++ app/services/transaction.py
@@ -3,17 +3,17 @@ import requests
 
 class TransactionService:
     def __init__(self, api_url: str, api_key: str):
-        self.api_url = api_url
-        self.api_key = api_key
+        self.sonicscan_api_url = api_url
+        self.sonicscan_api_key = api_key
 
     def get_transaction_details(self, transaction_hash: str) -> str:
         params = {
             "module": "proxy",
             "action": "eth_getTransactionByHash",
             "txhash": transaction_hash,
-            "apikey": self.api_key,
+            "apikey": self.sonicscan_api_key,
         }
-        response = requests.get(self.api_url, params=params)
+        response = requests.get(self.sonicscan_api_url, params=params)
         return response.json()
 
     def get_transaction_receipt(self, transaction_hash: str) -> str:
@@ -21,7 +21,7 @@ class TransactionService:
             "module": "transaction",
             "action": "gettxreceiptstatus",
             "txhash": transaction_hash,
-            "apikey": self.api_key,
+            "apikey": self.sonicscan_api_key,
         }
-        response = requests.get(self.api_url, params=params)
+        response = requests.get(self.sonicscan_api_url, params=params)
         return response.json()
